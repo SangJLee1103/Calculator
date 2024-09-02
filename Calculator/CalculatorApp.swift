d4fb141950828579be4e4cf64131d4787b90a0ce
이상준 <>
2024-09-02 22:03:13 +0900

    init: 프로젝트 세팅

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 이상준 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
